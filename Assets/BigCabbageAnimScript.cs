e8523b7ba6fb91e32fcd3e3cbb18df933a9b456a
khooroko <khoowp25@hotmail.com>
2017-11-19 19:46:29 +0800

    Make big cabbage animated

---
+++ Assets/BigCabbageAnimScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigCabbageAnimScript : MonoBehaviour {
+
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        anim.SetBool("isMoving", true);
+        anim.speed = 2f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
