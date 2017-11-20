7d87ab9fc059c46b4e1b9d7cd4730017f110efab
khooroko <khoowp25@hotmail.com>
2017-11-20 11:24:21 +0800

    Further optimise stage 2

--- Assets/Scripts/Level 2/SpawnManager.cs
+++ Assets/Scripts/Level 2/SpawnManager.cs
@@ -20,77 +20,77 @@ public class SpawnManager : MonoBehaviour {
             yield return new WaitForSeconds(2.0f);
             
             //Wave 1
-            go = Instantiate(cabbage, new Vector2(9f, 4f), Quaternion.identity) as GameObject;
+            go = Instantiate(cabbage, new Vector2(13f, 4f), Quaternion.identity) as GameObject;
             go.SendMessage("CreateWith", 12110);
-            go = Instantiate(cabbage, new Vector2(9f, 0f), Quaternion.identity) as GameObject;
+            go = Instantiate(cabbage, new Vector2(13f, 0f), Quaternion.identity) as GameObject;
             go.SendMessage("CreateWith", 20110);
-            go = Instantiate(cabbage, new Vector2(9f, -4f), Quaternion.identity) as GameObject;
+            go = Instantiate(cabbage, new Vector2(13f, -4f), Quaternion.identity) as GameObject;
             go.SendMessage("CreateWith", 12110);
 
             yield return new WaitForSeconds(1f);
-            go = Instantiate(shortSparkLink, new Vector2(9f, 4f), Quaternion.identity) as GameObject;
+            go = Instantiate(shortSparkLink, new Vector2(13f, 4f), Quaternion.identity) as GameObject;
             go.SendMessage("CreateWith", 900212);
             yield return new WaitForSeconds(1f);
-            go = Instantiate(shortSparkLink, new Vector2(9f, -4f), Quaternion.identity) as GameObject;
+            go = Instantiate(shortSparkLink, new Vector2(13f, -4f), Quaternion.identity) as GameObject;
             go.SendMessage("CreateWith", 450212);
             yield return new WaitForSeconds(1f);
-            go = Instantiate(longSparkLink, new Vector2(9f, 1f), Quaternion.identity) as GameObject;
+            go = Instantiate(longSparkLink, new Vector2(13f, 1f), Quaternion.identity) as GameObject;
             go.SendMessage("CreateWith", 000110);
             yield return new WaitForSeconds(1f);
-            go = Instantiate(longSparkLink, new Vector2(9f, -1f), Quaternion.identity) as GameObject;
+            go = Instantiate(longSparkLink, new Vector2(13f, -1f), Quaternion.identity) as GameObject;
             go.SendMessage("CreateWith", 200206);
 
-            go = Instantiate(cabbage, new Vector2(9f, 7f), Quaternion.identity) as GameObject;
+            go = Instantiate(cabbage, new Vector2(13f, 7f), Quaternion.identity) as GameObject;
             go.SendMessage("CreateWith", 110);
-            go = Instantiate(cabbage, new Vector2(9f, 4f), Quaternion.identity) as GameObject;
+            go = Instantiate(cabbage, new Vector2(13f, 4f), Quaternion.identity) as GameObject;
             go.SendMessage("CreateWith", 110);
-            go = Instantiate(cabbage, new Vector2(9f, 0f), Quaternion.identity) as GameObject;
+            go = Instantiate(cabbage, new Vector2(13f, 0f), Quaternion.identity) as GameObject;
             go.SendMessage("CreateWith", 110);
-            go = Instantiate(cabbage, new Vector2(9f, -4f), Quaternion.identity) as GameObject;
+            go = Instantiate(cabbage, new Vector2(13f, -4f), Quaternion.identity) as GameObject;
             go.SendMessage("CreateWith", 110);
-            go = Instantiate(cabbage, new Vector2(9f, -7f), Quaternion.identity) as GameObject;
+            go = Instantiate(cabbage, new Vector2(13f, -7f), Quaternion.identity) as GameObject;
             go.SendMessage("CreateWith", 110);
 
             //Wave 2
             yield return new WaitForSeconds(3f);
-            go = Instantiate(cabbage, new Vector2(9f, 7f), Quaternion.identity) as GameObject;
+            go = Instantiate(cabbage, new Vector2(13f, 7f), Quaternion.identity) as GameObject;
             go.SendMessage("CreateWith", 10110);
             go = Instantiate(shortSparkLink, new Vector2(6f, 9f), Quaternion.Euler(0f, 0f, 90f)) as GameObject;
             go.SendMessage("CreateWith", 000305);
             yield return new WaitForSeconds(1f);
-            go = Instantiate(cabbage, new Vector2(9f, 2f), Quaternion.identity) as GameObject;
-            go.SendMessage("CreateWith", 10110);
-            go = Instantiate(shortSparkLink, new Vector2(6f, 9f), Quaternion.Euler(0f, 0f, 90f)) as GameObject;
-            go.SendMessage("CreateWith", 000305);
-            go = Instantiate(shortSparkLink, new Vector2(-6f, -9f), Quaternion.Euler(0f, 0f, 90f)) as GameObject;
-            go.SendMessage("CreateWith", 000305);
-            yield return new WaitForSeconds(1f);
-            go = Instantiate(cabbage, new Vector2(9f, -2f), Quaternion.identity) as GameObject;
+            go = Instantiate(cabbage, new Vector2(13f, 2f), Quaternion.identity) as GameObject;
             go.SendMessage("CreateWith", 10110);
             go = Instantiate(shortSparkLink, new Vector2(6f, 9f), Quaternion.Euler(0f, 0f, 90f)) as GameObject;
             go.SendMessage("CreateWith", 000305);
             go = Instantiate(shortSparkLink, new Vector2(-6f, -9f), Quaternion.Euler(0f, 0f, 90f)) as GameObject;
             go.SendMessage("CreateWith", 000305);
             yield return new WaitForSeconds(1f);
-            go = Instantiate(cabbage, new Vector2(9f, -7f), Quaternion.identity) as GameObject;
+            go = Instantiate(cabbage, new Vector2(13f, -2f), Quaternion.identity) as GameObject;
             go.SendMessage("CreateWith", 10110);
             go = Instantiate(shortSparkLink, new Vector2(6f, 9f), Quaternion.Euler(0f, 0f, 90f)) as GameObject;
             go.SendMessage("CreateWith", 000305);
             go = Instantiate(shortSparkLink, new Vector2(-6f, -9f), Quaternion.Euler(0f, 0f, 90f)) as GameObject;
             go.SendMessage("CreateWith", 000305);
             yield return new WaitForSeconds(1f);
-            go = Instantiate(cabbage, new Vector2(9f, 2f), Quaternion.identity) as GameObject;
-            go.SendMessage("CreateWith", 10110);
-            go = Instantiate(cabbage, new Vector2(9f, -2f), Quaternion.identity) as GameObject;
+            go = Instantiate(cabbage, new Vector2(13f, -7f), Quaternion.identity) as GameObject;
             go.SendMessage("CreateWith", 10110);
             go = Instantiate(shortSparkLink, new Vector2(6f, 9f), Quaternion.Euler(0f, 0f, 90f)) as GameObject;
             go.SendMessage("CreateWith", 000305);
             go = Instantiate(shortSparkLink, new Vector2(-6f, -9f), Quaternion.Euler(0f, 0f, 90f)) as GameObject;
             go.SendMessage("CreateWith", 000305);
             yield return new WaitForSeconds(1f);
-            go = Instantiate(cabbage, new Vector2(9f, 5f), Quaternion.identity) as GameObject;
+            go = Instantiate(cabbage, new Vector2(13f, 2f), Quaternion.identity) as GameObject;
             go.SendMessage("CreateWith", 10110);
-            go = Instantiate(cabbage, new Vector2(9f, -5f), Quaternion.identity) as GameObject;
+            go = Instantiate(cabbage, new Vector2(13f, -2f), Quaternion.identity) as GameObject;
+            go.SendMessage("CreateWith", 10110);
+            go = Instantiate(shortSparkLink, new Vector2(6f, 9f), Quaternion.Euler(0f, 0f, 90f)) as GameObject;
+            go.SendMessage("CreateWith", 000305);
+            go = Instantiate(shortSparkLink, new Vector2(-6f, -9f), Quaternion.Euler(0f, 0f, 90f)) as GameObject;
+            go.SendMessage("CreateWith", 000305);
+            yield return new WaitForSeconds(1f);
+            go = Instantiate(cabbage, new Vector2(13f, 5f), Quaternion.identity) as GameObject;
+            go.SendMessage("CreateWith", 10110);
+            go = Instantiate(cabbage, new Vector2(13f, -5f), Quaternion.identity) as GameObject;
             go.SendMessage("CreateWith", 10110);
             go = Instantiate(shortSparkLink, new Vector2(6f, 9f), Quaternion.Euler(0f, 0f, 90f)) as GameObject;
             go.SendMessage("CreateWith", 000305);
@@ -99,7 +99,7 @@ public class SpawnManager : MonoBehaviour {
 
             //Wave 3
             yield return new WaitForSeconds(3f);
-            go = Instantiate(longSparkLink, new Vector2(9f, 2f), Quaternion.identity) as GameObject;
+            go = Instantiate(longSparkLink, new Vector2(13f, 2f), Quaternion.identity) as GameObject;
             go.SendMessage("CreateWith", 000110);
             yield return new WaitForSeconds(1f);
             go = Instantiate(cabbage, new Vector2(7f, 9f), Quaternion.identity) as GameObject;
@@ -110,7 +110,7 @@ public class SpawnManager : MonoBehaviour {
             yield return new WaitForSeconds(1f);
             go = Instantiate(cabbage, new Vector2(3f, 9f), Quaternion.identity) as GameObject;
             go.SendMessage("CreateWith", 10210);
-            go = Instantiate(longSparkLink, new Vector2(9f, -2f), Quaternion.identity) as GameObject;
+            go = Instantiate(longSparkLink, new Vector2(13f, -2f), Quaternion.identity) as GameObject;
             go.SendMessage("CreateWith", 000110);
             yield return new WaitForSeconds(1f);
             go = Instantiate(cabbage, new Vector2(1f, -9f), Quaternion.identity) as GameObject;
@@ -118,9 +118,9 @@ public class SpawnManager : MonoBehaviour {
             yield return new WaitForSeconds(1f);
             go = Instantiate(cabbage, new Vector2(-1f, 9f), Quaternion.identity) as GameObject;
             go.SendMessage("CreateWith", 10210);
-            go = Instantiate(shortSparkLink, new Vector2(9f, -5f), Quaternion.identity) as GameObject;
+            go = Instantiate(shortSparkLink, new Vector2(13f, -5f), Quaternion.identity) as GameObject;
             go.SendMessage("CreateWith", 000110);
-            go = Instantiate(shortSparkLink, new Vector2(9f, 5f), Quaternion.identity) as GameObject;
+            go = Instantiate(shortSparkLink, new Vector2(13f, 5f), Quaternion.identity) as GameObject;
             go.SendMessage("CreateWith", 000110);
             yield return new WaitForSeconds(1f);
             go = Instantiate(cabbage, new Vector2(-3f, -9f), Quaternion.identity) as GameObject;
@@ -128,7 +128,7 @@ public class SpawnManager : MonoBehaviour {
             yield return new WaitForSeconds(1f);
             go = Instantiate(cabbage, new Vector2(-5f, 9f), Quaternion.identity) as GameObject;
             go.SendMessage("CreateWith", 10210);
-            go = Instantiate(longSparkLink, new Vector2(9f, 0f), Quaternion.identity) as GameObject;
+            go = Instantiate(longSparkLink, new Vector2(13f, 0f), Quaternion.identity) as GameObject;
             go.SendMessage("CreateWith", 000110);
             yield return new WaitForSeconds(0.5f);
             go = Instantiate(cabbage, new Vector2(-2f, -9f), Quaternion.identity) as GameObject;
@@ -142,7 +142,7 @@ public class SpawnManager : MonoBehaviour {
             yield return new WaitForSeconds(0.5f);
             go = Instantiate(cabbage, new Vector2(4f, 9f), Quaternion.identity) as GameObject;
             go.SendMessage("CreateWith", 10210);
-            go = Instantiate(longSparkLink, new Vector2(9f, -3f), Quaternion.identity) as GameObject;
+            go = Instantiate(longSparkLink, new Vector2(13f, -3f), Quaternion.identity) as GameObject;
             go.SendMessage("CreateWith", 000110);
             yield return new WaitForSeconds(0.5f);
             go = Instantiate(cabbage, new Vector2(6f, -9f), Quaternion.identity) as GameObject;
@@ -158,7 +158,7 @@ public class SpawnManager : MonoBehaviour {
             yield return new WaitForSeconds(0.5f);
             go = Instantiate(shortSparkLink, new Vector2(0f, 9f), Quaternion.Euler(0f, 0f, 90f)) as GameObject;
             go.SendMessage("CreateWith", 000305);
-            go = Instantiate(cabbage, new Vector2(9f, 4f), Quaternion.identity) as GameObject;
+            go = Instantiate(cabbage, new Vector2(13f, 4f), Quaternion.identity) as GameObject;
             go.SendMessage("CreateWith", 12115);
             yield return new WaitForSeconds(0.5f);
             go = Instantiate(shortSparkLink, new Vector2(-3f, 9f), Quaternion.Euler(0f, 0f, 90f)) as GameObject;
@@ -168,7 +168,7 @@ public class SpawnManager : MonoBehaviour {
             go.SendMessage("CreateWith", 000305);
             go = Instantiate(shortSparkLink, new Vector2(6f, 9f), Quaternion.Euler(0f, 0f, 90f)) as GameObject;
             go.SendMessage("CreateWith", 000305);
-            go = Instantiate(cabbage, new Vector2(9f, -1f), Quaternion.identity) as GameObject;
+            go = Instantiate(cabbage, new Vector2(13f, -1f), Quaternion.identity) as GameObject;
             go.SendMessage("CreateWith", 12115);
             yield return new WaitForSeconds(0.5f);
             go = Instantiate(shortSparkLink, new Vector2(-4f, 9f), Quaternion.Euler(0f, 0f, 90f)) as GameObject;
@@ -179,7 +179,7 @@ public class SpawnManager : MonoBehaviour {
             yield return new WaitForSeconds(0.5f);
             go = Instantiate(shortSparkLink, new Vector2(2f, 9f), Quaternion.Euler(0f, 0f, 90f)) as GameObject;
             go.SendMessage("CreateWith", 000305);
-            go = Instantiate(cabbage, new Vector2(9f, 1f), Quaternion.identity) as GameObject;
+            go = Instantiate(cabbage, new Vector2(13f, 1f), Quaternion.identity) as GameObject;
             go.SendMessage("CreateWith", 12115);
             yield return new WaitForSeconds(1f);
             go = Instantiate(shortSparkLink, new Vector2(-6f, 9f), Quaternion.Euler(0f, 0f, 90f)) as GameObject;
@@ -189,7 +189,7 @@ public class SpawnManager : MonoBehaviour {
             yield return new WaitForSeconds(0.5f);
             go = Instantiate(shortSparkLink, new Vector2(4f, 9f), Quaternion.Euler(0f, 0f, 90f)) as GameObject;
             go.SendMessage("CreateWith", 000305);
-            go = Instantiate(cabbage, new Vector2(9f, 4f), Quaternion.identity) as GameObject;
+            go = Instantiate(cabbage, new Vector2(13f, 4f), Quaternion.identity) as GameObject;
             go.SendMessage("CreateWith", 12115);
             yield return new WaitForSeconds(0.5f);
             go = Instantiate(shortSparkLink, new Vector2(0f, 9f), Quaternion.Euler(0f, 0f, 90f)) as GameObject;
@@ -197,9 +197,9 @@ public class SpawnManager : MonoBehaviour {
             yield return new WaitForSeconds(0.5f);
             go = Instantiate(shortSparkLink, new Vector2(-4f, 9f), Quaternion.Euler(0f, 0f, 90f)) as GameObject;
             go.SendMessage("CreateWith", 000305);
-            go = Instantiate(cabbage, new Vector2(9f, 3f), Quaternion.identity) as GameObject;
+            go = Instantiate(cabbage, new Vector2(13f, 3f), Quaternion.identity) as GameObject;
             go.SendMessage("CreateWith", 32110);
-            go = Instantiate(cabbage, new Vector2(9f, -3f), Quaternion.identity) as GameObject;
+            go = Instantiate(cabbage, new Vector2(13f, -3f), Quaternion.identity) as GameObject;
             go.SendMessage("CreateWith", 01110);
 
 
@@ -208,7 +208,7 @@ public class SpawnManager : MonoBehaviour {
         }
         if(StaticCheckpoint.checkpoint == 1) {
             yield return new WaitForSeconds(2f);
-            go = Instantiate(boss, new Vector2(14f, 3f), Quaternion.identity) as GameObject;
+            go = Instantiate(boss, new Vector2(14f, 1f), Quaternion.identity) as GameObject;
         }
         if (StaticCheckpoint.checkpoint == 2) {
             yield return new WaitForSeconds(2f);
