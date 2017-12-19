ff89ac2403eba737d7f1e7a841b72c6d6de5825b
khooroko <khoowp25@hotmail.com>
2017-12-19 17:27:11 +0800

    Allow mouse input in start menu

--- Assets/Scripts/Start Menu/StartMenuMain.cs
+++ Assets/Scripts/Start Menu/StartMenuMain.cs
@@ -9,8 +9,7 @@ public class StartMenuMain : MonoBehaviour {
     private Animator anim;
     private float inputLag = 0.2f;
     private float lastCommandTime;
-    private int sceneToLoad1;
-    private int sceneToLoad2;
+    private int sceneToLoad;
     public GameObject[] clearedPics;
 
 	// Use this for initialization
@@ -31,69 +30,50 @@ public class StartMenuMain : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.Space))
-            Debug.Log(state);
-
         if (state == 0) {
-            if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return)) {
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return) || Input.GetMouseButtonDown(0)) {
                 anim.SetTrigger("Space1");
                 state = 1;
                 lastCommandTime = Time.time;
             }
         }
         if (state == 1) {
-            if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return)) && Time.time > lastCommandTime + inputLag) {
+            if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return) || Input.GetMouseButtonDown(0)) && Time.time > lastCommandTime + inputLag) {
                 anim.SetTrigger("Space2");
                 state = 2;
                 lastCommandTime = Time.time;
             }
         }
         if (state == 2) {
-            if (Input.GetAxis("Vertical") > 0.1f && Time.time > lastCommandTime + inputLag) {
-                anim.SetTrigger("Up");
+            if (Mathf.Abs(Input.GetAxis("Vertical")) > 0.1f && Time.time > lastCommandTime + inputLag) {
                 lastCommandTime = Time.time;
-                sceneToLoad1 += 1;
-                sceneToLoad1 %= 2;
+                sceneToLoad = (sceneToLoad == 0) ? 2 : sceneToLoad == 2 ? 0 : sceneToLoad == 1 ? 3 : 1;
+                anim.SetInteger("Level", sceneToLoad + 1);
             }
-
-            if (Input.GetAxis("Vertical") < -0.1f && Time.time > lastCommandTime + inputLag) {
-                anim.SetTrigger("Down");
+            if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0.1f && Time.time > lastCommandTime + inputLag) {
                 lastCommandTime = Time.time;
-                sceneToLoad1 += 1;
-                sceneToLoad1 %= 2;
+                sceneToLoad = (sceneToLoad == 0) ? 1 : sceneToLoad == 1 ? 0 : sceneToLoad == 2 ? 3 : 2;
+                anim.SetInteger("Level", sceneToLoad + 1);
             }
-            if (Input.GetAxis("Horizontal") > 0.1f && Time.time > lastCommandTime + inputLag) {
-                anim.SetTrigger("Right");
-                lastCommandTime = Time.time;
-                sceneToLoad2 += 1;
-                sceneToLoad2 %= 2;
-            }
-            if (Input.GetAxis("Horizontal") < -0.1f && Time.time > lastCommandTime + inputLag) {
-                anim.SetTrigger("Left");
-                lastCommandTime = Time.time;
-                sceneToLoad2 += 1;
-                sceneToLoad2 %= 2;
-            }
-            if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return)) && Time.time > lastCommandTime + inputLag) {
-                if (sceneToLoad1 == 0) {
-                    if (sceneToLoad2 == 0)
-                        SceneManager.LoadScene("Level 1");
-                    else
-                        SceneManager.LoadScene("Level 2");
-                }
-                else {
-
-                    if (sceneToLoad2 == 0)
-                        SceneManager.LoadScene("Level 3");
-                    else
-                        SceneManager.LoadScene("Level 4");
-                }
+            if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return))) {
+                LoadSelectedScene();
             }
         }
         if (Input.GetKey(KeyCode.Backslash)) {
             for (int i = 1; i < clearedPics.Length; i++) {
                 clearedPics[i].SetActive(false);
             }
+        }
+    }
+
+    public void SetScene(int level) {
+        sceneToLoad = level - 1;
+        anim.SetInteger("Level", level);
+    }
+
+    public void LoadSelectedScene() {
+        if (state == 2 && Time.time > lastCommandTime + inputLag) {
+            SceneManager.LoadScene("Level " + (sceneToLoad + 1));
         }
     }
 }
